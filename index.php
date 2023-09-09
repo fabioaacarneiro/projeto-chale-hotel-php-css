482dd85bff3b41c6795187416b590f10ee2c0376
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-09 16:12:55 -0300

    projeto concluído, ainda falta implementações para visar reforço de aprendizado, mas a estrutura base está completa

--- index.php
+++ index.php
@@ -34,6 +34,65 @@
                 </div>
             </div>
         </div>
+        <div id="content-area">
+            <div id="main-area">
+                <div class="content">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat facilis quod odit quia repudiandae doloremque, asperiores rerum at alias hic aspernatur illum architecto modi velit ut cupiditate fuga. Minus, hic!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur laboriosam iusto explicabo quibusdam corporis nihil, minus illo facere sed temporibus ratione atque eligendi provident deleniti architecto modi, error veritatis consectetur.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet esse provident fugiat, quas consequuntur laborum nemo sint, doloribus, accusamus omnis nihil incidunt sunt quasi quos. Ad ut ipsa nobis exercitationem.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nulla velit, doloremque tenetur veniam tempore iste soluta eius alias ratione ipsa sapiente delectus dolores ipsum beatae voluptas officiis porro facilis.
+                    </p>
+                </div>
+            </div>
+            <div id="side-area">
+                <div class="content">
+                    <ul id="benefits">
+                        <li>
+                            <a href="">
+                                <img src="assets/img/apartamento.jpg" alt="apartamento">
+                                <h2>Apartamento</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deserunt alias, maxime non quod velit, praesentium accusantium natus repellat quidem ut quos veritatis, neque odit dicta obcaecati quo sapiente rerum.
+                                </p>
+                            </a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="">
+                                <img src="assets/img/restaurante.jpg" alt="restaurante" width="200">
+                                <h2>Restaurante</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi harum obcaecati corrupti, enim assumenda, reprehenderit beatae commodi architecto id aliquid omnis aperiam maiores quasi minima tenetur voluptatibus praesentium. Neque, accusantium!
+                                </p>
+                            </a>
+                        </li>
+                        <hr>
+                        <li>
+                            <a href="">
+                                <img src="assets/img/piscina.jpg" alt="piscina" width="200">
+                                <h2>Piscina</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut voluptatum animi. Quasi facilis tempore, deserunt praesentium odit aut! Voluptate quidem quam voluptatum molestias sequi blanditiis nesciunt debitis dicta sint.
+                                </p>
+                            </a>
+                        </li>
+                        <hr>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="footer">
+            &copy; Copyright <?php echo date('Y') ?> - Chalé Hotel por Fabio Carneiro
+        </div>
     </div>
 </body>
 </html>
